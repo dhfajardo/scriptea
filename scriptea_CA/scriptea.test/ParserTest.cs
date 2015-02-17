dabef8ad0c6605914cc72c7bf8d0f4a80b7e7a4c
dhfajardo <fajardoalan@gmail.com>
2015-02-16 19:31:48 -0600

    Parser
    Parsing 1.4

--- scriptea_CA/scriptea.test/ParserTest.cs
+++ scriptea_CA/scriptea.test/ParserTest.cs
@@ -11,42 +11,6 @@ namespace scriptea.test
     [TestClass]
     public class ParserTest
     {
-        /*[TestMethod]
-        //[ExpectedException(typeof(ParserException))]
-        public void TestOPDec()
-        {
-            var parser = new Parser(new Lexer(new InputStream("--")));
-            parser.StartINTerminal = new DecrementOperator();
-            parser.Parse();
-        }*/
-        [TestMethod]
-        //[ExpectedException(typeof(ParserException))]
-        public void TestOPDec()
-        {
-            var parser = new Parser(new Lexer(new InputStream("--")));
-            parser.StartINTerminal = new DecrementOperator();
-            parser.Parse();
-        }
-        [TestMethod]
-        public void TestOPInc()
-        {
-            var parser = new Parser(new Lexer(new InputStream("++")));
-            parser.StartINTerminal = new IncrementOperator();
-            parser.Parse();
-        }
-        [TestMethod]
-        public void TestOpUnary()
-        {
-            var parser = new Parser(new Lexer(new InputStream("~")));
-            parser.StartINTerminal = new UnaryOperator();
-            parser.Parse();
-        }
-        [TestMethod]
-        public void TestExPrimary()
-        {
-            var parser = new Parser(new Lexer(new InputStream("")));
-            parser.StartINTerminal = new PrimaryExpression();
-            parser.Parse();
-        }
+        
     }
 }
