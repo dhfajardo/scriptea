d2dd7818cd9a4aa4dcc4c047fce2857b0320d8b2
dhfajardo <fajardoalan@gmail.com>
2015-03-12 17:43:46 -0600

    Evaluate II

--- scriptea_CA/scriptea/Tree/SymbolTable.cs
+++ scriptea_CA/scriptea/Tree/SymbolTable.cs
@@ -12,17 +12,19 @@ namespace scriptea.Tree
 
         public void AddSymbol(string name, dynamic value)
         {
-            this._symbolTable.Add(name,value);
+            if (this._symbolTable.ContainsKey(name))
+            {
+                this._symbolTable[name] = value;
+            }
+            else
+            {
+                this._symbolTable.Add(name, value);
+            }
         }
 
         public dynamic GetSymbol(string name)
         {
             return this._symbolTable[name];
-        }
-
-        public void UpdateSymbol(string name, dynamic value)
-        {
-            this._symbolTable[name] = value;
         }
 
         public void DeleteSymbol(string name)
